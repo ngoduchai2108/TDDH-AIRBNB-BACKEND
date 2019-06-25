3c23ce3823b3ea9a61fe902f8985566e548a703f
admin1 <hai2108hatayqo@gmail.com>
2019-06-25 10:17:18 +0700

    fix

--- src/main/java/com/codegym/tddh/airbnb/repository/ImageHouseRepository.java
+++ src/main/java/com/codegym/tddh/airbnb/repository/ImageHouseRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ImageHouseRepository extends JpaRepository<Image,Long> {
-    List<Image> findAllByHouse(House house);
+//    List<Image> findAllByHouse(House house);
 }

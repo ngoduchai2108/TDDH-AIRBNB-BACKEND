6e117122c7a781b0db58742d3bb2bc8679524f12
minhtri3797 <minhtri3797@gmail.com>
2019-06-25 10:09:50 +0700

    model image

--- src/main/java/com/codegym/tddh/airbnb/model/Image.java
+++ src/main/java/com/codegym/tddh/airbnb/model/Image.java
@@ -15,6 +15,9 @@ public class Image {
     @Size(min = 3, max = 50)
     private String name;
 
+    @NotBlank
+    private  String type;
+
     @Lob
     private byte[] data;
 
@@ -25,9 +28,18 @@ public class Image {
     public Image() {
     }
 
-    public Image( String name, byte[] data) {
+    public Image( String name,  String type, byte[] data) {
         this.name = name;
+        this.type = type;
         this.data = data;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public Long getId() {

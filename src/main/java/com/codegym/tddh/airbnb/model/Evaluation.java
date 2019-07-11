7d639c4c47d4fa937bf08317934fbcbe645fbdff
dinh <dinh.kttn.neu@gmail.com>
2019-07-11 14:25:23 +0700

    Create Model Evaluation and Reply

---
+++ src/main/java/com/codegym/tddh/airbnb/model/Evaluation.java
@@ -0,0 +1,78 @@
+package com.codegym.tddh.airbnb.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Evaluation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Max(5)
+    @Min(1)
+    private int rating;
+
+    @NotBlank
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "house_id")
+    private House house;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Evaluation() {
+    }
+
+    public Evaluation(int rating, String comment) {
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public House getHouse() {
+        return house;
+    }
+
+    public void setHouse(House house) {
+        this.house = house;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
